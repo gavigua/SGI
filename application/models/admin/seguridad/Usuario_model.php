c1c0e0274fe9b6be0d26c35ea253236a78159308
Luis Villegas <luism.villegass@gmail.com>
2017-07-03 11:49:50 -0400

    Merge remote-tracking branch 'refs/remotes/origin/master' into Desarrollo
    # Conflicts:
    #	application/config/config.php

--- application/models/admin/seguridad/Usuario_model.php
+++ application/models/admin/seguridad/Usuario_model.php
@@ -1,7 +1,5 @@
 <?php
-
-defined('BASEPATH') or exit('No direct script access allowed');
-
+defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * Modelo que utiliza la libreria MY_Model para
  * la gestión de la tabla usuario.
@@ -19,143 +17,116 @@ defined('BASEPATH') or exit('No direct script access allowed');
  * @license         MIT
  * @since           31/06/2017
  */
-class Usuario_model extends MY_Model
-{
-
+class Usuario_model extends MY_Model {
     /**
      * Nombre de la tabla gestionada por éste modelo
      * @var string
      */
     public $_table = 'usuario';
-
     /**
      * Reglas de validación utilizadas
      * por la libreria MY_Model
      * para la inserción de datos en la tabla.
      * @var array
      */
-    public $validate = [
-
-        ['field' => 'usuario', 'label' => 'Usuario', 'rules' => 'trim|required|max_length[20]|unique[usuario.usuario]'],
-        ['field' => 'nombre', 'label' => 'Nombre', 'rules' => 'trim|required|max_length[20]'],
-        ['field' => 'apellido', 'label' => 'Apellido', 'rules' => 'trim|required|max_length[20]'],
-        ['field' => 'password', 'label' => 'Contraseña', 'rules' => 'trim|required|min_length[5]|max_length[20]'],
-        ['field' => 're_password', 'label' => 'Verifique Contraseña', 'rules' => 'trim|required|matches[password]'],
-        ['field' => 'estado', 'label' => 'Estado', 'rules' => 'trim|required|is_natural'],
-        ['field' => 'rol_id', 'label' => 'Rol', 'rules' => 'trim|required'],
-        ['field' => 'departamento_id', 'label' => 'Departamento', 'rules' => 'trim|required'],
-    ];
-
+    public $validate = array(
+        array('field' => 'avatar', 'label' => 'Avatar', 'rules' => 'trim|required'),
+        array('field' => 'usuario', 'label' => 'Usuario', 'rules' => 'trim|required|max_length[20]|unique[usuario.usuario]'),
+        array('field' => 'nombre', 'label' => 'Nombre', 'rules' => 'trim|required|max_length[20]'),
+        array('field' => 'apellido', 'label' => 'Apellido', 'rules' => 'trim|required|max_length[20]'),
+        array('field' => 'password', 'label' => 'Contraseña', 'rules' => 'trim|required|min_length[5]|max_length[20]'),
+        array('field' => 're_password', 'label' => 'Verifique Contraseña', 'rules' => 'trim|required|matches[password]'),
+        array('field' => 'estado', 'label' => 'Estado', 'rules' => 'trim|required|is_natural'),
+        array('field' => 'rol_id', 'label' => 'Rol', 'rules' => 'trim|required'),
+    );
     /**
      * Reglas de validación utilizadas
      * por la libreria MY_Model
      * para la actualización de datos en la tabla.
      * @var array
      */
-    public $validate_update = [
-
-        ['field' => 'usuario', 'label' => 'Usuario', 'rules' => 'trim|required|unique[usuario.usuario]'],
-        ['field' => 'nombre', 'label' => 'Nombre', 'rules' => 'trim|required|max_length[20]'],
-        ['field' => 'apellido', 'label' => 'Apellido', 'rules' => 'trim|required|max_length[20]'],
-        ['field' => 'password', 'label' => 'Contraseña', 'rules' => 'trim|min_length[5]|max_length[10]'],
-        ['field' => 're_password', 'label' => 'Verifique Contraseña', 'rules' => 'trim|min_length[5]|matches[password]'],
-        ['field' => 'estado', 'label' => 'Estado', 'rules' => 'trim|required|is_natural'],
-        ['field' => 'rol_id', 'label' => 'Rol', 'rules' => 'trim|required'],
-        ['field' => 'departamento_id', 'label' => 'Departamento', 'rules' => 'trim|required'],
-    ];
-
+    public $validate_update = array(
+        array('field' => 'avatar', 'label' => 'Avatar', 'rules' => 'trim|required'),
+        array('field' => 'usuario', 'label' => 'Usuario', 'rules' => 'trim|required|unique[usuario.usuario]'),
+        array('field' => 'nombre', 'label' => 'Nombre', 'rules' => 'trim|required|max_length[20]|unique[usuario.nombre]'),
+        array('field' => 'apellido', 'label' => 'Apellido', 'rules' => 'trim|required|max_length[20]|unique[usuario.apellido]'),
+        array('field' => 'password', 'label' => 'Contraseña', 'rules' => 'trim|min_length[5]|max_length[10]'),
+        array('field' => 're_password', 'label' => 'Verifique Contraseña', 'rules' => 'trim|min_length[5]|matches[password]'),
+        array('field' => 'estado', 'label' => 'Estado', 'rules' => 'trim|required|is_natural'),
+        array('field' => 'rol_id', 'label' => 'Rol', 'rules' => 'trim|required')
+    );
     /**
      * Este método consulta todos los usuarios
      * y su relación con la tabla usuario_rol
      * @return array Todos los grados
      */
-    public function getAll()
-    {
+    public function getAll() {
         return $this->db
-            ->select('U.*, UR.rol_id, R.descripcion')
-            ->from($this->_table . ' U')
-            ->join('usuario_rol UR', 'UR.usuario_id = U.id')
-            ->join('Rol R', 'R.id = UR.rol_id')
-            ->get()
-            ->result();
+                        ->select('U.*, UR.rol_id, R.descripcion')
+                        ->from($this->_table . ' U')
+                        ->join('usuario_rol UR', 'UR.usuario_id = U.id')
+                        ->join('Rol R', 'R.id = UR.rol_id')
+                        ->get()
+                        ->result();
     }
-
     /**
      * Retorna el registro del usuario solicitado,
      * @param Int $id id del usuario
      * @return String Array
      */
-    public function getDato($id)
-    {
+    public function getDato($id) {
         return $this->db
-            ->select('U.*, UR.rol_id')
-            ->from($this->_table . ' U')
-            ->join('usuario_rol UR', 'UR.usuario_id = U.id AND U.id= ' . $id)
-            ->get()
-            ->row();
+                        ->select('U.*, UR.rol_id')
+                        ->from($this->_table . ' U')
+                        ->join('usuario_rol UR', 'UR.usuario_id = U.id AND U.id= '.$id)
+                        ->get()
+                        ->row();
     }
-
-    /*
-     * SELECT U.*, UR.rol_id, UD.departamento_id
-     * FROM ((usuario U INNER JOIN usuario_rol UR ON  UR.usuario_id = U.id )
-     *  INNER JOIN usuario_departamento UD ON UD.usuario_id = U.id)
-     */
-
     /**
      * Se crea el usuario en la tabla usuario,
      * y su respectiva relación en las tablas:
      *  - usuario_rol
      * @return Boolean
      */
-    public function crear()
-    {
-        if ($this->input->post('rol_id') == 'SuperAdministrador') {
-            $avatar = 'admin.jpg';
-        } else {
-            $avatar = 'operador.jpg';
-        }
-        $data = [
-            'avatar' => $avatar,
+    public function crear() {
+        $data = array(
+            'avatar' => $this->input->post('avatar'),
             'usuario' => $this->input->post('usuario'),
             'nombre' => $this->input->post('nombre'),
             'apellido' => $this->input->post('apellido'),
             'email' => $this->input->post('email'),
             'password' => hash('sha256', sha1($this->input->post('password'))),
-            'estado' => $this->input->post('estado'),
-            'departamento_id' => $this->input->post('departamento_id'),
-        ];
+            'estado' => $this->input->post('estado')
+        );
         $this->db->insert('usuario', beforeInsert($data));
         $usuario_id = $this->db->insert_id();
         /*
          * Se crea el rol para la persona
          */
-        $this->db->insert('usuario_rol', beforeInsert(['usuario_id' => $usuario_id, 'rol_id' => $this->input->post('rol_id')]));
-
-        return true;
+        $this->db->insert('usuario_rol', beforeInsert(array('usuario_id' => $usuario_id, 'rol_id' => $this->input->post('rol_id'))));
+        return TRUE;
     }
-
     /**
      * Método para actualizar el registro del
      * usuario y reasignar.
      * @param Int $id
      * @return Boolean
      */
-    public function actualizar($id)
-    {
-        $data = [
+    public function actualizar($id) {
+        $data = array(
             'avatar' => $this->input->post('avatar'),
             'usuario' => $this->input->post('usuario'),
             'nombre' => $this->input->post('nombre'),
             'apellido' => $this->input->post('apellido'),
             'email' => $this->input->post('email'),
-            'estado' => $this->input->post('estado'),
-        ];
-        if ($this->input->post('password')) {
+            'estado' => $this->input->post('estado')
+        );
+        if ($this->input->post('password'))
             $data['password'] = hash('sha256', sha1($this->input->post('password')));
-        }
-
         $this->db->where('id', $id)->update('usuario', beforeUpdate($data));
-        $this->db->where('usuario_id', $id)->update('usuario_rol', beforeUpdate(['rol_id' => $this->input->post('rol_id')])); //se actualiza el rol
-        return true;
+        $this->db->where('usuario_id', $id)->update('usuario_rol', beforeUpdate(array('rol_id' => $this->input->post('rol_id')))); //se actualiza el rol
+        return TRUE;
     }
+
+
 }
